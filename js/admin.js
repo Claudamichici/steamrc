eccc402ea88a9ebbd1655bd6bf3ed048e0c69635
Sebastian Mosquera <sebimosquera@gmail.com>
2021-03-23 18:43:02 -0300

    se agrego el archivo juegosClass para empezar a trabajar la logica de la administracion

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,2 @@
+import {Juegos} from '.juegosClass.js';
 import {validarCodigo, validarNombreJuego, validarCategoria, validarDescripcion, validarPrecio, validarUrl } from './validaciones.js'
---
+++ js/juegosClass.js
@@ -0,0 +1,12 @@
+export class Juegos{
+    constructor(codigo, nombreDeJuego, categoria, descripcion, imagen, imagenDos, precio, url){
+        this.codigo = codigo;
+        this.nombreDeJuego = nombreDeJuego;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.imagenDos = imagenDos;
+        this.precio = precio;
+        this.url = url;
+    }
+} 

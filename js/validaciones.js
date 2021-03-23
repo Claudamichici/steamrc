a8199ceaf7459bfa83ec016d393ff2f4d43a78ba
Sebastian Mosquera <sebimosquera@gmail.com>
2021-03-23 10:47:24 -0300

    se agrego nuevas validaciones y nuevos inputs en la tabla de productos

--- js/validaciones.js
+++ js/validaciones.js
@@ -6,6 +6,10 @@ let inputCategoria = document.getElementById('categoria');
 inputCategoria.addEventListener('blur', validarCategoria);
 let inputDescripcion = document.getElementById('descripcion');
 inputDescripcion.addEventListener('blur', validarDescripcion);
+let inputPrecio = document.getElementById('precio');
+inputPrecio.addEventListener('blur', validarPrecio);
+let inputUrl = document.getElementById('url');
+inputUrl.addEventListener('blur', validarUrl);
 
 export function validarCodigo () {
     if(inputCodigo.value.trim() != '' && !isNaN (inputCodigo.value) && inputCodigo.value.length >= 2){
@@ -45,4 +49,26 @@ export function validarDescripcion () {
         inputDescripcion.className = 'form-control is-invalid'
         return false;
     }
+}
+
+export function validarPrecio () {
+    if(inputPrecio.value.trim() != '' && !isNaN (inputPrecio.value)){
+        inputPrecio.className = 'form-control is-valid'
+        return true;
+    }else{
+        inputPrecio.className = 'form-control is-invalid'
+        return false;
+    } 
+}
+
+export function validarUrl () {
+    let expresion = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi;
+    if(inputUrl.value.trim() != '' && expresion.test(inputUrl.value)){
+        inputUrl.className = 'form-control is-valid'
+        return true;
+    }else{
+        inputUrl.className = 'form-control is-invalid'
+        return false;
+    }
+
 }

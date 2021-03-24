9ce36804548bf22203d51506f0567627f69fd337
emalderete <nicolas.alderete95@live.com>
2021-03-23 23:56:55 -0300

    Se realizan correcciones ortegráficas en la página de nosotros, se añade la función para agregar producto y la logica de la ventana modal

--- js/juegosClass.js
+++ js/juegosClass.js
@@ -1,12 +1,12 @@
-export class Juegos{
-    constructor(codigo, nombreDeJuego, categoria, descripcion, imagen, imagenDos, precio, url){
+export class Juegos {
+    constructor(codigo, nombreDeJuego, categoria, descripcion, imagen1, imagen2, precio, url) {
         this.codigo = codigo;
         this.nombreDeJuego = nombreDeJuego;
         this.categoria = categoria;
         this.descripcion = descripcion;
-        this.imagen = imagen;
-        this.imagenDos = imagenDos;
+        this.imagen1 = imagen1;
+        this.imagen2 = imagen2;
         this.precio = precio;
         this.url = url;
     }
-} 
+}

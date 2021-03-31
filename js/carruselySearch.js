0f6bea028bc04b86e46233be8a888a0f4da3bc4d
Agustin LLebeili <dimitrix151@gmail.com>
2021-03-31 04:20:37 -0300

    Se agrego el boton de busqueda de juegos

---
+++ js/carruselySearch.js
@@ -0,0 +1,35 @@
+function flechaderecha(contenedorCarusel){
+    let fila = document.getElementById(contenedorCarusel)
+    fila.scrollLeft += fila.offsetWidth;
+}
+
+function flechaizquierda(contenedorCarusel){
+    let fila = document.getElementById(contenedorCarusel)
+    fila.scrollLeft -= fila.offsetWidth;
+}
+
+const formulario = document.querySelector('#formulario');
+const boton = document.querySelector('#boton');
+const resultado = document.querySelector('#resultados')
+
+let filtrar = ()=>{
+    //console.log(listaJuegos);
+    resultado.innerHTML = '';
+    const texto = formulario.value.toLowerCase();
+    for(let producto of listaJuegos){
+        let nombre = producto.nombreDeJuego.toLowerCase();
+        if(nombre.indexOf(texto) !== -1){
+            resultado.innerHTML += `<a class="text-light" href="#" onclick="abrirDetalles(this.id)" id="${producto.codigo}">${producto.nombreDeJuego} <br></a>`
+        };
+    };
+
+    if(resultado.innerHTML === ''){
+        resultado.innerHTML += `<li>Producto no encontrado...</li>`
+    }
+}
+
+boton.addEventListener('click', filtrar);
+
+//formulario.addEventListener('keyup', filtrar);
+
+//filtrar();
